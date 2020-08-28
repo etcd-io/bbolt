ea42d394b8266f7679155be6dfb89557fe1b7d11
adream307 <adream307@163.com>
2020-08-28 11:10:49 +0800

    remove unreachable code

--- db.go
+++ db.go
@@ -1129,7 +1129,7 @@ func (m *meta) validate() error {
 		return ErrInvalid
 	} else if m.version != version {
 		return ErrVersionMismatch
-	} else if m.checksum != 0 && m.checksum != m.sum64() {
+	} else if m.checksum != m.sum64() {
 		return ErrChecksum
 	}
 	return nil
