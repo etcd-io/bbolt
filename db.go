2ea708af0554e2c12b86b71cd91164f57f3df59f
Seebs <seebs@molecula.com>
2021-09-30 12:16:53 -0500

    make NoSync more effective
    The database init and grow operations still used fsync even when
    NoSync was set, on the grounds that these operations are comparatively
    rare and it makes sense to want them to be more reliable even at
    some marginal cost in speed.
    
    But if you're doing tests on something which happens to *use*
    boltdb, this can result in hundreds and hundreds of fsyncs, possibly
    in parallel, and that becomes expensive.

--- db.go
+++ db.go
@@ -505,8 +505,10 @@ func (db *DB) init() error {
 	if _, err := db.ops.writeAt(buf, 0); err != nil {
 		return err
 	}
-	if err := fdatasync(db); err != nil {
-		return err
+	if !db.NoSync || IgnoreNoSync {
+		if err := fdatasync(db); err != nil {
+			return err
+		}
 	}
 	db.filesz = len(buf)
 
@@ -1017,8 +1019,10 @@ func (db *DB) grow(sz int) error {
 				return fmt.Errorf("file resize error: %s", err)
 			}
 		}
-		if err := db.file.Sync(); err != nil {
-			return fmt.Errorf("file sync error: %s", err)
+		if !db.NoSync || IgnoreNoSync {
+			if err := db.file.Sync(); err != nil {
+				return fmt.Errorf("file sync error: %s", err)
+			}
 		}
 		if db.Mlock {
 			// unlock old file and lock new one
