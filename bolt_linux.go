14bc5f958c2565e6e35763b1716da8f08a6ad0b8
Benjamin Wang <benjamin.ahrtr@gmail.com>
2023-11-19 13:02:42 +0000

    update fdatasync log
    Signed-off-by: Benjamin Wang <benjamin.ahrtr@gmail.com>

--- bolt_linux.go
+++ bolt_linux.go
@@ -7,9 +7,10 @@ import (
 
 // fdatasync flushes written data to a file descriptor.
 func fdatasync(db *DB) error {
+	fmt.Printf("linux: fdatasync started\n")
 	err := syscall.Fdatasync(int(db.file.Fd()))
 	if err != nil {
-		fmt.Printf("linux: fdatasync failed: %v", err)
+		fmt.Printf("linux: fdatasync failed: %v\n", err)
 	}
 	return err
 }
