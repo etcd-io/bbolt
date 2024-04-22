45153f878660477b270ce0877ef56a4108c7f5c6
Mustafa Elbehery <melbeher@redhat.com>
2024-04-22 20:43:25 +0200

    add pageId flag to check cmd
    Signed-off-by: Mustafa Elbehery <melbeher@redhat.com>
    Signed-off-by: samuelbartels20 <bartelssamuel20@gmail.com>

--- cmd/bbolt/command_check_test.go
+++ cmd/bbolt/command_check_test.go
@@ -9,25 +9,58 @@ import (
 
 	main "go.etcd.io/bbolt/cmd/bbolt"
 	"go.etcd.io/bbolt/internal/btesting"
+	"go.etcd.io/bbolt/internal/guts_cli"
 )
 
 func TestCheckCommand_Run(t *testing.T) {
-	db := btesting.MustCreateDB(t)
-	db.Close()
-	defer requireDBNoChange(t, dbData(t, db.Path()), db.Path())
+	testCases := []struct {
+		name      string
+		args      []string
+		expErr    error
+		expOutput string
+	}{
+		{
+			name:      "check whole db",
+			args:      []string{"check", "path"},
+			expErr:    nil,
+			expOutput: "OK\n",
+		},
+		{
+			name:      "check valid pageId",
+			args:      []string{"check", "path", "--from-page", "3"},
+			expErr:    nil,
+			expOutput: "OK\n",
+		},
+		{
+			name:      "check invalid pageId",
+			args:      []string{"check", "path", "--from-page", "1"},
+			expErr:    guts_cli.ErrCorrupt,
+			expOutput: "page ID (1) out of range [2, 4)",
+		},
+	}
 
-	rootCmd := main.NewRootCommand()
-	// capture output for assertion
-	outputBuf := bytes.NewBufferString("")
-	rootCmd.SetOut(outputBuf)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
 
-	rootCmd.SetArgs([]string{
-		"check", db.Path(),
-	})
-	err := rootCmd.Execute()
-	require.NoError(t, err)
+			t.Log("Creating sample DB")
+			db := btesting.MustCreateDB(t)
+			db.Close()
+			defer requireDBNoChange(t, dbData(t, db.Path()), db.Path())
 
-	output, err := io.ReadAll(outputBuf)
-	require.NoError(t, err)
-	require.Equalf(t, "OK\n", string(output), "unexpected stdout:\n\n%s", string(output))
+			t.Log("Running check cmd")
+			rootCmd := main.NewRootCommand()
+			outputBuf := bytes.NewBufferString("") // capture output for assertion
+			rootCmd.SetOut(outputBuf)
+
+			tc.args[1] = db.Path() // path to be replaced with db.Path()
+			rootCmd.SetArgs(tc.args)
+			err := rootCmd.Execute()
+			require.Equal(t, tc.expErr, err)
+
+			t.Log("Checking output")
+			output, err := io.ReadAll(outputBuf)
+			require.NoError(t, err)
+			require.Containsf(t, string(output), tc.expOutput, "unexpected stdout:\n\n%s", string(output))
+		})
+	}
 }
