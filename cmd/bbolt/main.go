93d08dec976cdd065b4ab07b456a484d88cd7ee6
Mustafa Elbehery <melbeher@redhat.com>
2025-09-09 21:27:56 +0200

    refactor: remove unused errors
    Signed-off-by: Mustafa Elbehery <melbeher@redhat.com>

--- cmd/bbolt/main.go
+++ cmd/bbolt/main.go
@@ -7,25 +7,11 @@ import (
 )
 
 var (
-	// ErrUsage is returned when a usage message was printed and the process
-	// should simply exit with an error.
-	ErrUsage = errors.New("usage")
-
-	// ErrUnknownCommand is returned when a CLI command is not specified.
-	ErrUnknownCommand = errors.New("unknown command")
-
 	// ErrPathRequired is returned when the path to a Bolt database is not specified.
 	ErrPathRequired = errors.New("path required")
 
-	// ErrFileNotFound is returned when a Bolt database does not exist.
-	ErrFileNotFound = errors.New("file not found")
-
 	// ErrInvalidValue is returned when a benchmark reads an unexpected value.
 	ErrInvalidValue = errors.New("invalid value")
-
-	// ErrNonDivisibleBatchSize is returned when the batch size can't be evenly
-	// divided by the iteration count.
-	ErrNonDivisibleBatchSize = errors.New("number of iterations must be divisible by the batch size")
 
 	// ErrPageIDRequired is returned when a required page id is not specified.
 	ErrPageIDRequired = errors.New("page id required")
@@ -38,9 +24,6 @@ var (
 
 	// ErrKeyNotFound is returned when a key is not found.
 	ErrKeyNotFound = errors.New("key not found")
-
-	// ErrNotEnoughArgs is returned with a cmd is being executed with fewer arguments.
-	ErrNotEnoughArgs = errors.New("not enough arguments")
 )
 
 func main() {
